0bc79c75399c8f2d4841a1b97d19f85b436ef208
Andrey Bartenev <59123372+MrRobinGoood@users.noreply.github.com>
2022-12-10 19:54:36 +0400

    Create bubble and choice sorting. All test clear.

---
+++ json-processing/src/test/java/org/example/SortingTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SortingTest {
+
+    Sorting sortBubble = new ExampleA();
+    @Test
+    void sortBubble(){
+        Assertions.assertArrayEquals(sortBubble.sort(new int[]{5,3,4,2,1}), new int[]{1,2,3,4,5});
+        Assertions.assertArrayEquals(sortBubble.sort(new int[]{}), new int[]{});
+        NullPointerException excNull = Assertions.assertThrows(NullPointerException.class, () -> {sortBubble.sort(null);});
+        Assertions.assertEquals(excNull.getMessage(),"Array is null" );
+    }
+    Sorting sortChoice = new ExampleB();
+    @Test
+    void sortChoice(){
+        Assertions.assertArrayEquals(sortChoice.sort(new int[]{5,3,4,2,1}), new int[]{1,2,3,4,5});
+        Assertions.assertArrayEquals(sortChoice.sort(new int[]{}), new int[]{});
+        NullPointerException excNull = Assertions.assertThrows(NullPointerException.class, () -> {sortChoice.sort(null);});
+        Assertions.assertEquals(excNull.getMessage(),"Array is null" );
+    }
+}
