b31d559bb331873983113dcaacc400c95a89b473
Andrey Bartenev <59123372+MrRobinGoood@users.noreply.github.com>
2022-12-11 20:33:02 +0400

    Add spring boot to project.

---
+++ spring-learn/src/test/java/ru/nshi/springlearn/SpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nshi.springlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
